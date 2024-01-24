e07d7fb027ac7731740e0ac9db168777b40e1244
Edy <drobeduard@gmail.com>
2024-01-25 01:48:32 +0200

    Clean up

--- components/Discog/Discog.jsx
+++ components/Discog/Discog.jsx
@@ -7,14 +7,7 @@ import { useNavigate, Navigate } from "react-router-dom"
 
 function Discog() {
 
-    // function getToken() {
-    //     const tokenString = sessionStorage.getItem('token')
-    //     return tokenString
-    // }
     
-    
-    
-    //use http://127.0.0.1:8081/recommendation/discogs?discogsToken=&discogsTokenSecret=&pageNumber=4&numberOfItemsPerPage=1
  
     const navigate = useNavigate()
     const [showVinyls, setShowVinyls] = useState(false);
