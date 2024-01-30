355f833f0f294e590ccaba8a24044b4fe571437d
Drob Cristian-Eduard <44617359+HOOOD1N1@users.noreply.github.com>
2024-01-31 00:09:54 +0200

    Update env.js

--- public/env.js
+++ public/env.js
@@ -1,5 +1,5 @@
 window.env = {
   "REDIRECT_URI": "http://localhost:8080/",
-  "CLIENT_ID": "fa7c98bd00864cd38932e9c8e45d81d6",
-  "CLIENT_SECRET": "68ae700b2dc04f70b58599ab9e9bffbc"
+  "CLIENT_ID": "",
+  "CLIENT_SECRET": ""
 };
